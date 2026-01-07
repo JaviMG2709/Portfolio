a50818cb0200ca3c91a0183ee13c93481702870b
Javier Moreno <146024535+JaviMG2709@users.noreply.github.com>
2026-01-08 00:33:42 +0100

    Arreglo de estilos

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://javimg2709.github.io/Portfolio/',
