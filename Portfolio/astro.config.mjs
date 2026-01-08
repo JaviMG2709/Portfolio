ca4637e59f445a65b74077e26b17a65542f07443
Javier Moreno <146024535+JaviMG2709@users.noreply.github.com>
2026-01-08 13:23:12 +0100

    Arreglo

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    site: 'https://javimg2709.github.io/Portfolio/',
-    vite: {
-        plugins: [tailwindcss()],
-    },
+    site: 'https://javimg2709.github.io',
+    base: '/Portfolio/',
+    vite: { plugins: [tailwindcss()] },
 });
